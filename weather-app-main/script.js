6d780b8837adc5297d0d742479fd301906c95d1f
devdrogba <aderogbasamueladeolu@gmail.com>
2025-11-05 20:23:46 +0100

    update

--- weather-app-main/script.js
+++ weather-app-main/script.js
@@ -61,21 +61,20 @@ console.log(condition)
   }).catch((error) => console.log(error))
 }
 
-// === Reverse geocode to get city & country ===
+// === Reverse geocode to get city & country =
 function reverseGeocode(lat, lon) {
-  const geoUrl = `https://geocoding-api.open-meteo.com/v1/reverse?latitude=${lat}&longitude=${lon}`;
-  https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json
-console.log(geoUrl)
+  const geoUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`;
+
   fetch(geoUrl)
     .then((response) => response.json())
     .then((data) => {
-      const place = data.results?.[0];
-      if (place) {
-        document.getElementById("location").innerText = `${place.name}, ${place.country}`;
-      }
+      const city = data.city || data.locality || "Unknown City";
+      const country = data.countryName || "Unknown Country";
+      document.getElementById("location").innerText = `${city}, ${country}`;
+      console.log(`${city}, ${country}`);
     })
-    .catch((error) => console.log("Could not fetch location name.",error));
-
+    .catch((error) =>
+      console.error("Could not fetch location name.", error)
+    );
 }
-
 //getWeather(6.5244, 3.3792, true); // Lagos coordinates
